3f07add07d0034633f7af3e639e5cd82aaf1822c
mariaeloisaa <mariaeloisafortunato@hotmail.com>
2024-12-11 14:38:19 -0300

    front

---
+++ front/scripts/database.js
@@ -0,0 +1,356 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Regador Amarelo", 
+        preco: 25.00, 
+        descricao:"Regador minimalista, decorativo e funcional para você dar banho nas suas plantinhas...",
+        imagemProduto: {
+            img1:"./images/products/prod01/regador.webp",
+            img2:"./images/products/prod01/regador.webp",
+            img3:"./images/products/prod01/regador.webp",
+            img4:"./images/products/prod01/regador.webp",
+
+        },
+        categoriaProduto: 'Ferramentas/Acessórios',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Kit Ferramentas para Jardinagem", 
+        preco: 120.50, 
+        descricao:"Kit Ferramentas para jardinagem.",
+        imagemProduto: {
+            img1:"./images/products/prod02/ferramentas.jpg",
+            img2:"./images/products/prod02/ferramentas.jpg",
+            img3:"./images/products/prod02/ferramentas.jpg",
+            img4:"./images/products/prod02/ferramentas.jpg",
+    },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Kit 3 Vasinhos Criativos para suculentas", 
+        preco: 79.90, 
+        descricao:"Kit 3 Vasinhos criativos de dinosssauro, jacaré e elefante fofinhos para suas suculentas!",
+        imagemProduto: {
+            img1:"./images/products/prod03/vaso_2.jpg",
+            img2:"./images/products/prod03/vaso_2.jpg",
+            img3:"./images/products/prod03/vaso_2.jpg",
+            img4:"./images/products/prod03/vaso_2.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:4,
+        tituloProduto:"Guia Prático de Jardinagem", 
+        preco: 24.90, 
+        descricao:"Guia prático de Jardinagem para você que deseja adiquirir conhecimento e cuidar melhor das suas plantinhas.",
+        imagemProduto: {
+            img1:"./images/products/prod04/guia.jpg",
+            img2:"./images/products/prod04/guia.jpg",
+            img3:"./images/products/prod04/guia.jpg",
+            img4:"./images/products/prod04/guia.jpg",
+
+        },
+        categoriaProduto: 'Guias',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:5,
+        tituloProduto:"Vasinho Criativo Cafumelo", 
+        preco: 32.50, 
+        descricao:"Vaso decorativo de Cogumelo + Café, para você que ama café tanto quanto ama suas plantinhas.",
+        imagemProduto: {
+            img1:"./images/products/prod05/vaso_cogumelo.webp",
+            img2:"./images/products/prod05/vaso_cogumelo.webp",
+            img3:"./images/products/prod05/vaso_cogumelo.webp",
+            img4:"./images/products/prod05/vaso_cogumelo.webp",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:6,
+        tituloProduto:"Umidificador de Ambiente", 
+        preco: 40.99, 
+        descricao:"Umidificador de Ambiente para as suas plantinhas que gostam de um ar mais úmido. ",
+        imagemProduto: {
+            img1:"./images/products/prod06/umidificador.jpg",
+            img2:"./images/products/prod06/umidificador.jpg",
+            img3:"./images/products/prod06/umidificador.jpg",
+            img4:"./images/products/prod06/umidificador.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:7,
+        tituloProduto:"Trio Vasinhos Minimalistas", 
+        preco: 55.50, 
+        descricao:"Um Kit 3 vasinhos minimalistas, para você que busca a natureza na modernidade",
+        imagemProduto: {
+            img1:"./images/products/prod07/vaso1.jpg",
+            img2:"./images/products/prod07/vaso1.jpg",
+            img3:"./images/products/prod07/vaso1.jpg",
+            img4:"./images/products/prod07/vaso1.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:8,
+        tituloProduto:"Vaso Baby Polvo", 
+        preco: 35.50, 
+        descricao:"Vasinho criativo de polvo, para você que ama coisas criativas!",
+        imagemProduto: {
+            img1:"./images/products/prod08/vaso2.jpg",
+            img2:"./images/products/prod08/vaso2.jpg",
+            img3:"./images/products/prod08/vaso2.jpg",
+            img4:"./images/products/prod08/vaso2.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:9,
+        tituloProduto:"Vaso de Bichinho", 
+        preco: 42.90, 
+        descricao:"Vasinho criativo e fofo de raposa para sua suculenta linda!",
+        imagemProduto: {
+            img1:"./images/products/prod09/vaso9.jpg",
+            img2:"./images/products/prod09/vaso9.jpg",
+            img3:"./images/products/prod09/vaso9.jpg",
+            img4:"./images/products/prod09/vaso9.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:10,
+        tituloProduto:"Kit Vasos Ceramica", 
+        preco: 55.80, 
+        descricao:"3 Vasinhos de ceramica modernos para a suas plantinhas!",
+        imagemProduto: {
+            img1:"./images/products/prod10/vaso4.jpg",
+            img2:"./images/products/prod10/vaso4.jpg",
+            img3:"./images/products/prod10/vaso4.jpg",
+            img4:"./images/products/prod10/vaso4.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:11,
+        tituloProduto:"Kit Vasos Cactos", 
+        preco: 30.00, 
+        descricao:"2 Vasinhos de Cacto para seus Cactos!",
+        imagemProduto: {
+            img1:"./images/products/prod11/vaso5.avif",
+            img2:"./images/products/prod11/vaso5.avif",
+            img3:"./images/products/prod11/vaso5.avif",
+            img4:"./images/products/prod11/vaso5.avif",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:12,
+        tituloProduto:"Vaso Aquário", 
+        preco: 64.99, 
+        descricao:"Vaso aquário para sua planta e seu peixinho, economize espaço sendo criativo! Ah, lembre-se de colocar um peixinho beta, pois se adapta ao espaço...",
+        imagemProduto: {
+            img1:"./images/products/prod12/vaso6.jpg",
+            img2:"./images/products/prod12/vaso6.jpg",
+            img3:"./images/products/prod12/vaso6.jpg",
+            img4:"./images/products/prod12/vaso6.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:13,
+        tituloProduto:"Vaso Pato", 
+        preco: 24.99, 
+        descricao:"Vaso em formato de Pato criativo e divertido...",
+        imagemProduto: {
+            img1:"./images/products/prod13/vaso13.jpg",
+            img2:"./images/products/prod13/vaso13.jpg",
+            img3:"./images/products/prod13/vaso13.jpg",
+            img4:"./images/products/prod13/vaso13.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:14,
+        tituloProduto:"Vaso Romântico Flores", 
+        preco: 27.99, 
+        descricao:"Vaso Romântico e Delicado.",
+        imagemProduto: {
+            img1:"./images/products/prod14/vaso14.jpg",
+            img2:"./images/products/prod14/vaso14.jpg",
+            img3:"./images/products/prod14/vaso14.jpg",
+            img4:"./images/products/prod14/vaso14.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:15,
+        tituloProduto:"Vaso Moderno", 
+        preco: 27.90, 
+        descricao:"Vaso abstrato para você que ama modernidade!",
+        imagemProduto: {
+            img1:"./images/products/prod15/vaso15.jpg",
+            img2:"./images/products/prod15/vaso15.jpg",
+            img3:"./images/products/prod15/vaso15.jpg",
+            img4:"./images/products/prod15/vaso15.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:16,
+        tituloProduto:"Vaso Saquinho Presente", 
+        preco: 30.50, 
+        descricao:"Vaso em forma de saquinho de presente com laço, para você presentear alguém que ama!",
+        imagemProduto: {
+            img1:"./images/products/prod16/vaso16.jpg",
+            img2:"./images/products/prod16/vaso16.jpg",
+            img3:"./images/products/prod16/vaso16.jpg",
+            img4:"./images/products/prod16/vaso16.jpg",
+
+        },
+        categoriaProduto: 'Vasos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:17,
+        tituloProduto:"Regador Cerâmica Moderno", 
+        preco: 60.90, 
+        descricao:"Regador de Creâmica moderno e elegante, funcional e decorativo para transformar seu cantinho de jardinagem!",
+        imagemProduto: {
+            img1:"./images/products/prod17/regador17.jpg",
+            img2:"./images/products/prod17/regador17.jpg",
+            img3:"./images/products/prod17/regador17.jpg",
+            img4:"./images/products/prod17/regador17.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas/Acessórios',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:18,
+        tituloProduto:"Kit Completo Jardinagem", 
+        preco: 130.80, 
+        descricao:"Kit compacto e completo de jardinagem, para você que quer cuidar das suas plantas e não tem muito espaço.",
+        imagemProduto: {
+            img1:"./images/products/prod18/kit18.jpg",
+            img2:"./images/products/prod18/kit18.jpg",
+            img3:"./images/products/prod18/kit18.jpg",
+            img4:"./images/products/prod18/kit18.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:19,
+        tituloProduto:"Suporte Corda para Plantas de Teto", 
+        preco: 54.80, 
+        descricao:"Suporte rede rústico e atrativo para pendurar suas plantinhas pendentes sem perder o charme!",
+        imagemProduto: {
+            img1:"./images/products/prod19/suporte19.jpg",
+            img2:"./images/products/prod19/suporte19.jpg",
+            img3:"./images/products/prod19/suporte19.jpg",
+            img4:"./images/products/prod19/suporte19.jpg",
+
+        },
+        categoriaProduto: 'Decoracoes',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:20,
+        tituloProduto:"Livro Jardim Secreto", 
+        preco: 60.00, 
+        descricao:"Livro Jardim Secreto, capa dura 16 junho 2021 por Frances Hodgson Burnett. Para você que ama a natureza até nas páginas.",
+        imagemProduto: {
+            img1:"./images/products/prod20/livro20.jpg",
+            img2:"./images/products/prod20/livro20.jpg",
+            img3:"./images/products/prod20/livro20.jpg",
+            img4:"./images/products/prod20/livro20.jpg",
+
+        },
+        categoriaProduto: 'Guias',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:21,
+        tituloProduto:"Kit Sementes de Flores", 
+        preco: 22.50, 
+        descricao:"Kit sementes de vários tipos de flores pra você que deseja viver uma nova experiência e passar todas as etapas do seu crescimnto.",
+        imagemProduto: {
+            img1:"./images/products/prod21/sementes21.jpg",
+            img2:"./images/products/prod21/sementes21.jpg",
+            img3:"./images/products/prod21/sementes21.jpg",
+            img4:"./images/products/prod21/sementes21.jpg",
+        },
+        categoriaProduto: 'Sementes',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    
+
+]
